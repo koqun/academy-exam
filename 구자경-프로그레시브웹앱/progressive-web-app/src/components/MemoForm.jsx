30714305c08ed303fa17ac721c1f4bdcc5392051
jagyeong gu <plzkoiu0912@naver.com>
2022-06-21 16:02:47 +0900

    react-redux init

---
+++ 구자경-프로그레시브웹앱/progressive-web-app/src/components/MemoForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addMemo } from "../store";
+
+export default function MemoForm() {
+  const [title, setTitle] = useState("");
+  const [memo, setMemo] = useState("");
+  const dispatch = useDispatch();
+
+  const subMemo = (e) => {
+    e.preventDefault();
+    dispatch(addMemo({ title: title, memo: memo }));
+    setTitle("");
+    setMemo("");
+  };
+
+  const titleHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const memoHandler = (e) => {
+    setMemo(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>메모하는 공간입니다</h1>
+      <form onSubmit={subMemo}>
+        <input type="text" onChange={titleHandler} value={title} />
+        <br></br>
+        <textarea
+          cols="30"
+          rows="10"
+          onChange={memoHandler}
+          value={memo}
+        ></textarea>
+        <br></br>
+        <input type="submit" value="입력" />
+      </form>
+    </>
+  );
+}
---
+++ 구자경-프로그레시브웹앱/progressive-web-app/src/store.js
@@ -0,0 +1,31 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const memoSlice = createSlice({
+  initialState: {
+    memoList: [{ id: 0, title: "첫번째", memo: "첫번째 메모입니다" }],
+    id: 1,
+  },
+  reducers: {
+    addMemo(state, action) {
+      state.memoList = state.memoList.concat({
+        ...action.payload,
+        id: state.id,
+      });
+      state.id += 1;
+    },
+    deleteMemo(state, action) {
+      const i = state.memoList.findIndex((el) => el.id === action.payload);
+      state.memoList.splice(i, 1);
+    },
+  },
+});
+
+export const { addMemo, deleteMemo } = memoSlice.actions;
+
+const store = configureStore({
+  reducer: {
+    memo: memoSlice.reducer,
+  },
+});
+
+export default store;
